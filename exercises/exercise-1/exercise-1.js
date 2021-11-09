515aca929beebb0cb48310614943909446907f3f
donna <dotdashhenry@gmail.com>
2021-11-09 12:33:42 +0000

    Exercise one complete

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -4,10 +4,12 @@ const personOne = {
   favouriteFood: "Spinach",
 };
 
-function introduceYourself(___________________________) {
+function introduceYourself({name, age, favouriteFood}) {   //do not forget the {}
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
 introduceYourself(personOne);
+
+
